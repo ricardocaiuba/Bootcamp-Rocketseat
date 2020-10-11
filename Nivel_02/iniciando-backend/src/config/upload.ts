4045ebc6058593b536e91ee338469bc5fa76c7b6
ricardocaiuba <ricardo.caiuba@gmail.com>
2020-10-11 10:13:38 -0300

    Atualizaando avatar

--- Nivel_02/iniciando-backend/src/config/upload.ts
+++ Nivel_02/iniciando-backend/src/config/upload.ts
@@ -2,9 +2,13 @@ import path from "path";
 import crypto from "crypto";
 import multer from "multer";
 
+const tmpFolter = path.resolve(__dirname, "..", "..", "tmp");
+
 export default {
+  directory: tmpFolter,
+
   storage: multer.diskStorage({
-    destination: path.resolve(__dirname, "..", "..", "tmp"),
+    destination: tmpFolter,
     filename(request, file, callback) {
       const fileHash = crypto.randomBytes(10).toString("hex");
       const fileName = `${fileHash}-${file.originalname}`;
