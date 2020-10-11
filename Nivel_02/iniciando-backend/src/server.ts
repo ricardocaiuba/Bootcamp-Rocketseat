5d2297cebbbdb7b8d519262bd180a577a69e053a
ricardocaiuba <ricardo.caiuba@gmail.com>
2020-10-11 11:00:52 -0300

    Lidando com erros

--- Nivel_02/iniciando-backend/src/server.ts
+++ Nivel_02/iniciando-backend/src/server.ts
@@ -1,9 +1,12 @@
 import "reflect-metadata";
 
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
+
 import cors from "cors";
 import routes from "./routes";
 import uploadConfig from "./config/upload";
+import AppError from "./errors/AppError";
 
 import "./database";
 
@@ -12,6 +15,21 @@ app.use(express.json());
 app.use("/files", express.static(uploadConfig.directory));
 app.use(cors());
 app.use(routes);
+
+app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      status: "error",
+      message: err.message,
+    });
+  }
+
+  console.log(err);
+  return res.status(500).json({
+    status: "error",
+    message: "Internal server error",
+  });
+});
 
 const port = 3333;
 
